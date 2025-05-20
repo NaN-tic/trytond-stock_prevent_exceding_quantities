0ccaae18e67ea0ccf48b068ad9ec30f71280f1ad
Raimon Esteve <raimonesteve@gmail.com>
2025-05-20 12:53:20 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -46,8 +46,18 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = [get_require_version('proteus')]
+tests_require = [
+    get_require_version('proteus'),
+]
+
+series = '%s.%s' % (major_version, minor_version)
+if minor_version % 2:
+    branch = 'default'
+else:
+    branch = series
+
 dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
